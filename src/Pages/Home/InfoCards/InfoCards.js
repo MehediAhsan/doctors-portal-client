c75f0e59b06e67cb86ea3d81dbf17b33473c1918
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-11-13 00:08:35 +0600

    added footer testimonial and appointment

--- src/Pages/Home/InfoCards/InfoCards.js
+++ src/Pages/Home/InfoCards/InfoCards.js
@@ -30,7 +30,7 @@ const InfoCards = () => {
     ]
 
     return (
-        <div className='grid mt-8 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             {
                 cardData.map(card => <InfoCard
                     key={card.id}
