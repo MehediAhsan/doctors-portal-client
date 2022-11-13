5f78fc94505dee0d6285e59b72da9f9b6ded77fb
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-11-13 22:39:28 +0600

    appointment page designed

---
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -0,0 +1,40 @@
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
+import AppointmentOption from './AppointmentOption';
+
+const AvailableAppointments = ({selectedDate}) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
+    useEffect(() => {
+        fetch('appointmentOptions.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }, [])
+
+    return (
+        <section className='my-24'>
+            <p className='text-center text-secondary font-medium text-xl pb-14'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 pb-20'>
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointmentOption={option}
+                        setTreatment={setTreatment}
+                    ></AppointmentOption>)
+                }
+            </div>
+            {
+                treatment &&
+                <BookingModal
+                    selectedDate={selectedDate}
+                    treatment={treatment}
+                    setTreatment={setTreatment}
+                ></BookingModal>
+            }
+        </section>
+    );
+};
+
+export default AvailableAppointments;
